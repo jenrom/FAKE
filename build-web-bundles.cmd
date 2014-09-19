56b625b30bd3c7545be011d42fa7e72a9a7827ff
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2014-09-19 15:11:38 +0200

    Automatic paket conversion

--- build-web-bundles.cmd
+++ build-web-bundles.cmd
@@ -1,26 +1,19 @@
 @echo off
 
-:Build
 cls
 
-"tools\nuget\nuget.exe" "install" "FAKE" "-OutputDirectory" "tools" "-ExcludeVersion" "-Prerelease"
+paket.bootstrapper.exe
+if errorlevel 1 (
+  exit /b %errorlevel%
+)
+
+paket.exe install -v
+if errorlevel 1 (
+  exit /b %errorlevel%
+)
 
 SET TARGET="Default"
 
 IF NOT [%1]==[] (set TARGET="%1")
-  
-"tools\FAKE\tools\Fake.exe" "build-web-bundles.fsx" "target=%TARGET%"
 
-rem Bail if we're running a TeamCity build.
-if defined TEAMCITY_PROJECT_NAME goto Quit
-
-rem Loop the build script.
-set CHOICE=nothing
-echo (Q)uit, (Enter) runs the build again
-set /P CHOICE= 
-if /i "%CHOICE%"=="Q" goto :Quit
-
-GOTO Build
-
-:Quit
-exit /b %errorlevel%
+"packages\FAKE\tools\Fake.exe" "build-web-bundles.fsx" "target=%TARGET%"
